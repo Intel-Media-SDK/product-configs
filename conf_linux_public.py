d4ed57cf2e3226ad38a32cad508c5e408451e2f5
Dydychkin <alexander.dydychkin@intel.com>
2018-05-14 14:40:27 +0300

    Add dynamic API_VERSION and BUILD_NUMBER counters and minor codestyle changes

--- conf_linux_public.py
+++ conf_linux_public.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Intel Corporation
+# Copyright (c) 2018 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,52 +18,104 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import re #TODO: Move libraries to the shared module
+import pathlib
+
+import git
+
+#TODO: move functions to the shared module
+def get_commit_number(repo_path):
+    git_repo = git.Git(repo_path)
+    commits = git_repo.log('--all', '--oneline')
+    return len(commits.splitlines())
+
+def get_api_version(repo_path):
+    """
+    :param name: Path to the MediaSDK folder
+    :type name: String or Path
+
+    Function finds the lines like:
+        `#define MFX_VERSION_MAJOR 1`
+        `#define MFX_VERSION_MINOR 26`
+    And prints the version like:
+        `1.26`
+    """
+    file_path = pathlib.Path(repo_path) / 'api' / 'include' / 'mfxdefs.h'
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            if re.search("MFX_VERSION_MAJOR\s(\d+)", line):
+                major_version = re.search("MFX_VERSION_MAJOR\s(\d+)", line).group(1)
+                minor_version = re.search("MFX_VERSION_MINOR\s(\d+)", next(file)).group(1)
+                return f"{major_version}.{minor_version}"
+
 
 PRODUCT_REPOS = [
     {'name': 'MediaSDK'},
     #{'name': 'flow_test'},
 ]
 
-ENABLE_DEVTOOLSET='source /opt/rh/devtoolset-6/enable'
+ENABLE_DEVTOOLSET = 'source /opt/rh/devtoolset-6/enable'
 
 MEDIA_SDK_REPO_DIR = DEFAULT_OPTIONS.get('REPOS_DIR') / PRODUCT_REPOS[0]['name']
-CMAKE_CFG = 'intel64.make.' + DEFAULT_OPTIONS.get('BUILD_TYPE')
 
+BUILD_NUMBER = get_commit_number(str(MEDIA_SDK_REPO_DIR))
+API_VERSION = get_api_version(MEDIA_SDK_REPO_DIR)
+PLUGIN_VERSION = f'{API_VERSION}.3.${BUILD_NUMBER}'
+
+BUILD_ENVIRONMENT = {
+    'MFX_HOME': str(MEDIA_SDK_REPO_DIR),
+    'BUILD_NUMBER': BUILD_NUMBER,
+    'MFX_VERSION': f'7.0.16093{BUILD_NUMBER}',
+    'MFX_HEVC_VERSION': f'{PLUGIN_VERSION}',
+    'MFX_H265FEI_VERSION': f'{PLUGIN_VERSION}',
+    'MFX_VP8_VERSION': f'{PLUGIN_VERSION}',
+    'MFX_VP9_VERSION': f'{PLUGIN_VERSION}',
+    'MFX_H264LA_VERSION': f'{PLUGIN_VERSION}',
+}
+
+CMAKE_CFG = 'intel64.make.' + DEFAULT_OPTIONS.get('BUILD_TYPE')
 DEFAULT_OPTIONS['BUILD_DIR'] = MEDIA_SDK_REPO_DIR / '__cmake' / CMAKE_CFG
 
-action('compiler version', cmd=f'{ENABLE_DEVTOOLSET} && gcc --version')
+action('compiler version',
+       cmd=f'{ENABLE_DEVTOOLSET} && gcc --version')
 
-action('cmake', cmd=f'{ENABLE_DEVTOOLSET} && perl tools/builder/build_mfx.pl --cmake={CMAKE_CFG}',
+action('cmake',
+       cmd=f'{ENABLE_DEVTOOLSET} && perl tools/builder/build_mfx.pl --cmake={CMAKE_CFG}',
        work_dir=MEDIA_SDK_REPO_DIR,
-       env={'MFX_HOME': str(MEDIA_SDK_REPO_DIR)})
+       env=BUILD_ENVIRONMENT)
 
-action('build', cmd=f'{ENABLE_DEVTOOLSET} && make -j{DEFAULT_OPTIONS["CPU_CORES"]}')
+action('build',
+       cmd=f'{ENABLE_DEVTOOLSET} && make -j{DEFAULT_OPTIONS["CPU_CORES"]}',
+       env=BUILD_ENVIRONMENT)
 
-action('install', stage=Stage.INSTALL, cmd=f'{ENABLE_DEVTOOLSET} && make DESTDIR={DEFAULT_OPTIONS["INSTALL_DIR"]} install'])
+action('install',
+       stage=Stage.INSTALL,
+       cmd=f'{ENABLE_DEVTOOLSET} && make DESTDIR={DEFAULT_OPTIONS["INSTALL_DIR"]} install')
 
 DEV_PKG_DATA_TO_ARCHIVE = [
+    {
+        'from_path': DEFAULT_OPTIONS['BUILD_DIR'],
+        'relative': [
             {
-                'from_path': DEFAULT_OPTIONS['BUILD_DIR'],
-                'relative': [
-                    {
-                        'path': '__bin',
-                        'pack_as': 'bin'
-                    },
-                    {
-                        'path': 'plugins.cfg',
-                        'pack_as': 'bin/release/plugins.cfg'
-                    }
-                ]
+                'path': '__bin',
+                'pack_as': 'bin'
+            },
+            {
+                'path': 'plugins.cfg',
+                'pack_as': 'bin/release/plugins.cfg'
             }
         ]
+    }
+]
 
 INSTALL_PKG_DATA_TO_ARCHIVE = [
+    {
+        'from_path': DEFAULT_OPTIONS['INSTALL_DIR'],
+        'relative': [
             {
-                'from_path': DEFAULT_OPTIONS['INSTALL_DIR'],
-                'relative': [
-                    {
-                        'path': 'opt'
-                    }
-                ]
+                'path': 'opt'
             }
         ]
+    }
+]
