60577c8f25d6542c2577cea51e146088ae36b067
Eugene Ponomarev <eugene.ponomarev@intel.com>
2018-09-19 14:18:06 +0300

    deploying infrastructure test

--- infrastructure_version.py
+++ infrastructure_version.py
@@ -1,1 +1,1 @@
-OPEN_SOURCE = {'branch': 'master', 'commit_id': '9a281912a788fa7c4adb6853d0795bc701c98f5a'}
+OPEN_SOURCE = {'branch': 'master', 'commit_id': '9a281912a788fa7c4adb6853d0795bc701c98f5a' }
