88b4d2af3401c9fd8f30acc17d6d4b52825d7db0
akharche <angelina.kharchevnikova@intel.com>
2018-09-27 17:19:52 +0300

    Fix check of defconfig builder

--- conf_linux_public.py
+++ conf_linux_public.py
@@ -120,7 +120,7 @@ else:
     cmake_command.append('-DCMAKE_CXX_FLAGS_RELEASE="-O2 -Wformat -Wformat-security -Wall -Werror -D_FORTIFY_SOURCE=2 -DNDEBUG -fstack-protector-strong"')
 
 #In all builders except Fastboot or clang build use parameter `-DENABLE_TOOLS=ON`:
-if 'no_x11' not in product_type and not args.get('fastboot') and not args.get('compiler') == "clang":
+if 'defconfig' not in product_type and not args.get('fastboot') and not args.get('compiler') == "clang":
     cmake_command.append('-DBUILD_ALL=ON')
     cmake_command.append('-DENABLE_ALL=ON')
 
