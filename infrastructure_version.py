c4bb8e3180c0753658f4d0aa1c3b90c848aaaaf6
Angelina Kharchevnikova <angelina.kharchevnikova@intel.com>
2018-10-12 16:40:25 +0300

    Test fix of failed hevc tests (#51)
    Test fix of failed hevc tests

--- infrastructure_version.py
+++ infrastructure_version.py
@@ -18,4 +18,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-OPEN_SOURCE = {'branch': 'master', 'commit_id': '9e763bf0a9ed67191ddc8588f9674062c7c99bc8'}
+OPEN_SOURCE = {'branch': 'master', 'commit_id': '3ea2f602ac8c26bbdaa26505a67d4a822c8b7b88'}
