4d76a3597aa2473f394f8aab3e87e2ded2f17a3b
Eugene Ponomarev <eugene.ponomarev@intel.com>
2019-04-02 15:17:07 +0300

    [Windows] Added building Debug configuration (#145)
    * [Windows] Added building Debug configuration
    
    * Package layout updated
    
    * Removed generator
    
    * Added TODO

--- conf_windows_public.py
+++ conf_windows_public.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2019 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 PRODUCT_REPOS = [
     {'name': 'MediaSDK'},
     # Give possibility to build windows for changes from product configs repository
@@ -11,6 +31,11 @@ BUILD_ENVIRONMENT = {
     'MSBuildEmitSolution': '1'
 }
 
+MSBUILD_ARGUMENTS = {
+    '/target': 'Build',
+    '/verbosity': 'minimal',
+    '/maxcpucount': options['CPU_CORES']
+}
 
 def clean_msbuild_dirs(repos_dir):
     import shutil
@@ -27,31 +52,33 @@ action(
 )
 
 for platform in ['x64', 'Win32']:
-    vs_component(
-        f"Build dispatcher (2015) {platform}",
-        solution_path=options['REPOS_DIR'] / r'MediaSDK\api\mfx_dispatch\windows\libmfx_vs2015.sln',
-        msbuild_args={
-            '/property': {
-                'Platform': platform
-            }
-        },
-        env=BUILD_ENVIRONMENT
-    )
+    # TODO: Release and/or Debug configurations should be previded by build scripts
+    for configuration in ['Release', 'Debug']:
+        vs_component(
+            f"Build dispatcher (2017) {platform}",
+            solution_path=options['REPOS_DIR'] / r'MediaSDK\api\mfx_dispatch\windows\libmfx_vs2015.sln',
+            msbuild_args={
+                '/property': {
+                    'Platform': platform,
+                    'Configuration': configuration
+                }
+            },
+            env=BUILD_ENVIRONMENT
+        )
 
-    INSTALL_PKG_DATA_TO_ARCHIVE.extend([{
-        'from_path': options['REPOS_DIR'] / 'build',
-        'relative': [
-            {
-                'path': rf'win_{platform}\lib\libmfx_vs2015_d.idb'
-            },
-            {
-                'path': rf'win_{platform}\lib\libmfx_vs2015_d.lib'
-            },
-            {
-                'path': rf'win_{platform}\lib\libmfx_vs2015_d.pdb'
-            }
-        ]
-    }])
+        install_package_file_suffixes = ['.lib', '.pdb']
+        if configuration == 'Debug':
+            install_package_file_suffixes.append('.idb')
+
+        # TODO: release and debug install packages should be separated
+        for suffix in install_package_file_suffixes:
+            INSTALL_PKG_DATA_TO_ARCHIVE.append({
+                'from_path': options['REPOS_DIR'] / 'build',
+                'relative': [
+                    {
+                        'path': rf'win_{platform}\lib\libmfx_vs2015{"" if configuration == "Release" else "_d"}{suffix}'
+                    }
+            ]})
 
     DEV_PKG_DATA_TO_ARCHIVE.extend([{
         'from_path': options['REPOS_DIR'] / 'build',
