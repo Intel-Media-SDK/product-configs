89280d2e607132618b587b75e4e9f6aa319ac860
Eugene Ponomarev <eugene.ponomarev@intel.com>
2019-04-16 12:05:34 +0300

    Test dependencies pipeline (#159)
    * Create tests.txt
    
    * Update infrastructure_version.py
    
    * Update infrastructure_version.py
    
    * Delete tests.txt

--- infrastructure_version.py
+++ infrastructure_version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Intel Corporation
+# Copyright (c) 2019 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +19,5 @@
 # SOFTWARE.
 
 
-OPEN_SOURCE = {'branch': 'master', 'commit_id': '88a887315bfe0e03854976fba099eb4c02a08060'}
+OPEN_SOURCE = {'branch': 'master', 'commit_id': '912c667f6cbfeee2c03d3235d5f3e166851a84a2'}
 
