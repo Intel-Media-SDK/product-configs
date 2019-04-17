6421e6d5de1e1360bafcdce3742c90f6ea6b6a2d
Eugene Ponomarev <eugene.ponomarev@intel.com>
2019-04-17 17:03:02 +0300

    Mss2018r2 dependencies (#162)
    * Added build config for media driver (#109)
    
    * Added build config for media driver
    
    * Switched to master
    
    * Created configuration for gmmlib build (#110)
    
    * [Git worker] Fail extraction if there is no branch or revision w/o retries (#111)
    
    * [Infra] Updates (#112)
    
    build_number returns number from "master" branch
    fix for extract_archive function
    
    * [Infra] git_worker updates (#113)
    
    get_commits method updates
    
    * Added full log of unit tests (#114)
    
    * Moved packing to build scripts (#91)
    
    * Update infrastructure version (#116)
    
    - Added check author name and email as pre commit check
    - Used branch like refs/pull/id/merge in pull request pollers
    
    * [Infra] Updates for github branches (#118)
    
    * [Infra] libva_conf updates (#119)
    
    + manifest was created;
    + 'tags/...' does not need;
    
    * [Infra] Manifest updates (#124)
    
    * Added libva-utils build (#125)
    
    * Enabled CI for media driver (#126)
    
    Updates:
    - Added prototype of buildbot configuration for driver (#209)
    - Added libva-utils repo (#212)
    - Disable fastboot build for mss2018_r2 branch (#211)
    - Removed commit-time argument for build script cmd (#208)
    
    * Update conf_media_driver.py (#127)
    
    * Update driver configuration (#128)
    
    * Removed MediaSDK repository
    
    * Added developer package
    
    * [Infrastructure] Add dependencies support (#131)
    
    Build scripts can work with manifest information
    
    * [Infrastructure] Add dependencies support (#132)
    
    Build scripts can work with manifest information
    
    * [Infra version] Enable manifests manager (#134)
    
    * [Infra version] Enable gitpoller for pull requests (#135)
    
    * Added folder for driver configs\files (#130)
    
    - Needed for easily identify changes in driver files for buildbot
    
    * [Driver] Set product name for updating layout of artifacts (#138)
    
    * Change config according requirements (#139)
    
    * Update config with required build option (#140)
    
    * [Manifest] Add product type to manifest (#141)
    
    * Created prototype config for intel-graphics-compiler build (#137)
    
    * [Infra version] Enabled notifications from buildbot for driver (#142)
    
    * [Windows] Added configuration for building dispatcher (#144)
    
    * [Driver] Add smoke test configuration (#143)
    
    * To update copyright checks for autogen files (#146)
    
    * [Windows] Added building Debug configuration (#145)
    
    * [Windows] Added building Debug configuration
    
    * Package layout updated
    
    * Removed generator
    
    * Added TODO
    
    * [Infra] Add test_runner.py for Driver (#149)
    
    + Runners update
    
    * [Driver] Show test output in Buildbot console (#150)
    
    * [Driver] Enabled fixes for tests (#151)
    
    * Added components to manifest (#136)
    
    * Added components to manifest
    
    * Fix in media-driver
    
    * Enabled full build log (#153)
    
    * Enabled full build log
    
    * Changes after review
    
    * Added configuration for OpenCL-runtime build (#154)
    
    * Added configuration for OpenCL-runtime build
    
    * Chenges after review
    
    * Change revision
    
    * [Windows] Added samples (#148)
    
    * [Windows] Added building samples
    
    * Test
    
    * Test with https://github.com/Intel-Media-SDK/MediaSDK/pull/1352/commits
    
    * Added TODO
    
    * Full stack for MediaSDK (#158)
    
    * Full stack for MediaSDK
    
    * Update versions
    
    * Exclude h264e_func_la test
    
    * Test dependencies pipeline (#159)
    
    * Create tests.txt
    
    * Update infrastructure_version.py
    
    * Update infrastructure_version.py
    
    * Delete tests.txt
    
    * Added building libva for mss2018_r2 release branch

---
+++ conf_gmmlib.py
@@ -0,0 +1,151 @@
+# Copyright (c) 2019 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pathlib import Path
+
+GMMLIB_REPO_NAME = 'gmmlib'
+PRODUCT_NAME = GMMLIB_REPO_NAME
+PRODUCT_CONFIGS_REPO_NAME = 'product-configs'
+
+GMMLIB_VERSION = manifest.get_component(GMMLIB_REPO_NAME).version
+GMMLIB_REPO_DIR = options.get('REPOS_DIR') / GMMLIB_REPO_NAME
+
+# Repos_to_extract
+PRODUCT_REPOS = [
+    {'name': GMMLIB_REPO_NAME},
+    {'name': PRODUCT_CONFIGS_REPO_NAME},
+]
+
+ENABLE_DEVTOOLSET = 'source /opt/rh/devtoolset-6/enable'
+# Workaround to run fpm tool on CentOS 6.9
+ENABLE_RUBY24 = 'source /opt/rh/rh-ruby24/enable'
+GCC_LATEST = '8.2.0'
+options["STRIP_BINARIES"] = True
+
+# By default install to the system
+# _DEB_PREFIX is used by default
+GMMLIB_DEB_PREFIX = Path('/usr/local')
+GMMLIB_CENTOS_PREFIX = Path('/usr')
+
+PKGCONFIG = 'lib64/pkgconfig'
+GMMLIB_PKGCONFIG_DIR = GMMLIB_DEB_PREFIX / PKGCONFIG
+
+GMMLIB_LIB_INSTALL_DIRS = {
+    'rpm': 'lib64',
+    'deb': 'lib/x86_64-linux-gnu'
+}
+
+
+#TODO: add more smart logic or warnings?! (potential danger zone)
+def get_building_cmd(command, gcc_latest, enable_devtoolset):
+     # Ubuntu Server: gcc_latest or clang
+    if args.get('compiler') == "clang" or (args.get('compiler') == "gcc" and args.get('compiler_version') == gcc_latest):
+        return command
+    else:
+        return f'{enable_devtoolset} && {command}' #enable new compiler on CentOS
+
+
+cmake_command = ['cmake3']
+cmake_command.append('-DCMAKE_SHARED_LINKER_FLAGS="-pie -z noexecstack -z relro -z now"')
+cmake_command.append(str(GMMLIB_REPO_DIR))
+cmake = ' '.join(cmake_command)
+
+# Build gmmlib
+action('gmmlib: cmake',
+       work_dir=options['BUILD_DIR'],
+       cmd=get_building_cmd(cmake, GCC_LATEST, ENABLE_DEVTOOLSET))
+
+action('gmmlib: build',
+       cmd=get_building_cmd(f'make -j`nproc`', GCC_LATEST, ENABLE_DEVTOOLSET))
+
+action('gmmlib: list artifacts',
+         cmd=f'echo " " && ls ./Source/GmmLib',
+         verbose=True)
+
+action('gmmlib: make install',
+       stage=stage.INSTALL,
+       work_dir=options['BUILD_DIR'],
+       cmd=get_building_cmd(f'make DESTDIR={options["INSTALL_DIR"]} install', GCC_LATEST, ENABLE_DEVTOOLSET))
+
+# gmmlib: pkgconfig for OS Ubuntu
+# Update pkgconfig prefix
+pkgconfig_deb_pattern = {
+    '/lib64': f"/{GMMLIB_LIB_INSTALL_DIRS['deb']}",
+}
+
+action('gmmlib: change pkgconfig for deb',
+       stage=stage.PACK,
+       callfunc=(update_config, [options["INSTALL_DIR"] / GMMLIB_DEB_PREFIX.relative_to(GMMLIB_DEB_PREFIX.root) / PKGCONFIG,
+                                 pkgconfig_deb_pattern], {}))
+
+# Get package installation dir for gmmlib
+pack_dir = options['INSTALL_DIR'] / GMMLIB_DEB_PREFIX.relative_to(GMMLIB_DEB_PREFIX.root)
+lib_install_to = GMMLIB_DEB_PREFIX / GMMLIB_LIB_INSTALL_DIRS['deb']
+include_install_to = GMMLIB_DEB_PREFIX
+
+GMMLIB_PACK_DIRS = [
+    f'{pack_dir}/lib64/={lib_install_to}/',
+    f'{pack_dir}/include/={include_install_to}/include',
+]
+
+action('gmmlib: create deb pkg',
+       stage=stage.PACK,
+       work_dir=options['PACK_DIR'],
+       cmd=get_packing_cmd('deb',  GMMLIB_PACK_DIRS, ENABLE_RUBY24, GMMLIB_VERSION, GMMLIB_REPO_NAME))
+
+# gmmlib: pkgconfig for OS CentOS
+# Update pkgconfig prefix
+pkgconfig_rpm_pattern = {
+    '^prefix=.+': 'prefix=/usr',
+    f'{GMMLIB_DEB_PREFIX}/include': f'{GMMLIB_CENTOS_PREFIX}/include',
+    f'{GMMLIB_DEB_PREFIX / GMMLIB_LIB_INSTALL_DIRS["deb"]}': f'{GMMLIB_CENTOS_PREFIX / GMMLIB_LIB_INSTALL_DIRS["rpm"]}',
+}
+
+action('gmmlib: change pkgconfigs for rpm',
+       stage=stage.PACK,
+       callfunc=(update_config, [options["INSTALL_DIR"] / GMMLIB_DEB_PREFIX.relative_to(GMMLIB_DEB_PREFIX.root) / PKGCONFIG,
+                                 pkgconfig_rpm_pattern], {}))
+
+# Get package installation dir for GMMLIB
+lib_install_to = GMMLIB_CENTOS_PREFIX / GMMLIB_LIB_INSTALL_DIRS['rpm']
+include_install_to = GMMLIB_CENTOS_PREFIX
+
+GMMLIB_PACK_DIRS = [
+    f'{pack_dir}/lib64/={lib_install_to}/',
+    f'{pack_dir}/include/={include_install_to}/include',
+]
+
+action('gmmlib: create rpm pkg',
+       stage=stage.PACK,
+       work_dir=options['PACK_DIR'],
+       cmd=get_packing_cmd('rpm', GMMLIB_PACK_DIRS, ENABLE_RUBY24, GMMLIB_VERSION, GMMLIB_REPO_NAME))
+
+
+# TODO: Define where to copy
+INSTALL_PKG_DATA_TO_ARCHIVE.extend([
+    {
+        'from_path': options['INSTALL_DIR'],
+        'relative': [
+            {
+                'path': 'usr',
+            }
+        ]
+    },
+])
---
+++ conf_windows_public.py
@@ -0,0 +1,129 @@
+# Copyright (c) 2019 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+PRODUCT_REPOS = [
+    {'name': 'MediaSDK'},
+    # Give possibility to build windows for changes from product configs repository
+    {'name': 'product-configs'}
+]
+
+BUILD_ENVIRONMENT = {
+    'INTELMEDIASDKROOT': str(options['REPOS_DIR'] / 'MediaSDK' / 'api'),
+    'MINIDDK_ROOT': r'C:\Program Files (x86)\Windows Kits\10',
+    'MINIDDK_VERSION': '10.0.17134.0',
+    'MSBuildEmitSolution': '1'
+}
+
+MSBUILD_ARGUMENTS = {
+    '/target': 'Build',
+    '/verbosity': 'minimal',
+    '/maxcpucount': options['CPU_CORES']
+}
+
+
+def clean_msbuild_dirs(repos_dir):
+    import shutil
+
+    build_dir = repos_dir / 'build'
+    if build_dir.exists():
+        log.info('remove directory %s', build_dir)
+        shutil.rmtree(build_dir)
+
+
+def dispatcher(platform='x64', configuration='Release', build_environment=BUILD_ENVIRONMENT):
+    vs_component(
+        f"Build dispatcher (2017) {platform} {configuration}",
+        solution_path=options['REPOS_DIR'] / r'MediaSDK\api\mfx_dispatch\windows\libmfx_vs2015.sln',
+        msbuild_args={
+            '/property': {
+                'Platform': platform,
+                'Configuration': configuration
+            }
+        },
+        env=build_environment
+    )
+
+    install_package_file_suffixes = ['.lib', '.pdb']
+    if configuration == 'Debug':
+        install_package_file_suffixes.append('.idb')
+
+    for suffix in install_package_file_suffixes:
+        INSTALL_PKG_DATA_TO_ARCHIVE.append({
+            'from_path': options['REPOS_DIR'] / 'build',
+            'relative': [
+                {
+                    'path': rf'win_{platform}\lib\libmfx_vs2015{"" if configuration == "Release" else "_d"}{suffix}'
+                }
+        ]})
+
+    dev_package = {
+        'from_path': options['REPOS_DIR'] / 'build',
+        'relative': [
+            {
+                'path': rf'win_{platform}'
+            }
+        ]
+    }
+
+    # Need to avoid multiple adding the same (win_{platform}) directory to developer package
+    if dev_package not in DEV_PKG_DATA_TO_ARCHIVE:
+        DEV_PKG_DATA_TO_ARCHIVE.append(dev_package)
+
+
+def samples(platform='x64', configuration='Release', build_environment=BUILD_ENVIRONMENT):
+    vs_component(
+        f"Build Samples {platform} {configuration}",
+        solution_path=options['REPOS_DIR'] / r'MediaSDK\samples\AllSamples.sln',
+        env=build_environment,
+        msbuild_args={
+            '/property': {
+                'Platform': platform,
+                'Configuration': configuration
+            }
+        }
+    )
+
+    DEV_PKG_DATA_TO_ARCHIVE.extend([{
+        'from_path': options['REPOS_DIR'] / 'MediaSDK' / 'samples' / '_build',
+        'relative': [
+            {
+                'path': rf'{platform}\{configuration}',
+                'pack_as': rf'samples\{platform}\{configuration}'
+            }
+        ]
+    }])
+
+action(
+    'Clean msbuild dirs',
+    stage=stage.CLEAN,
+    callfunc=(clean_msbuild_dirs, [options['REPOS_DIR']], {})
+)
+
+
+# TODO: add support for the build-type arg
+dispatcher(platform='x64', configuration='Release')
+dispatcher(platform='x64', configuration='Debug')
+dispatcher(platform='Win32', configuration='Release')
+dispatcher(platform='Win32', configuration='Debug')
+
+samples(platform='x64', configuration='Release')
+samples(platform='x64', configuration='Debug')
+samples(platform='Win32', configuration='Release')
+samples(platform='Win32', configuration='Debug')
