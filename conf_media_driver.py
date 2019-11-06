688a3888c699a7d28497f1b4efb180509a7a5579
dlobanox <dmitriyx.lobanov@intel.com>
2019-11-06 17:22:52 +0300

    [CI][Infra] Update Clang to version 9

--- conf_media_driver.py
+++ conf_media_driver.py
@@ -38,7 +38,7 @@ ENABLE_DEVTOOLSET = 'source /opt/rh/devtoolset-6/enable'
 # Workaround to run fpm tool on CentOS 6.9
 ENABLE_RUBY24 = 'source /opt/rh/rh-ruby24/enable'
 GCC_LATEST = '9.2.0'
-CLANG_VERSION = '8'
+CLANG_VERSION = '9'
 options["STRIP_BINARIES"] = True
 
 DRIVER_INSTALL_PREFIX = Path('/opt/intel/msdk_driver')
