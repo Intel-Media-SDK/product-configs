85404a01b8f723f3b30bfa796130d10dd39452a5
Eugene Ponomarev <eugene.ponomarev@intel.com>
2020-07-28 05:00:09 +0300

    [Metrics-calc] Replaced "_" to "-" in name and calls (#324)

--- conf_metrics_calc.py
+++ conf_metrics_calc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Intel Corporation
+# Copyright (c) 2019-2020 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,9 +21,9 @@
 from pathlib import Path
 
 CALC_REPO_NAME = 'tools'
-PRODUCT_NAME = 'metrics_calc_lite'
+PRODUCT_NAME = 'metrics-calc-lite'
 
-CALC_REPO_DIR = options.get('REPOS_DIR') / CALC_REPO_NAME / PRODUCT_NAME
+CALC_REPO_DIR = options.get('REPOS_DIR') / CALC_REPO_NAME / 'metrics_calc_lite'
 BUILD_NUM = get_commit_number(CALC_REPO_DIR)
 CALC_VERSION = manifest.get_component(PRODUCT_NAME).version + f'.{BUILD_NUM}'
 
